c5439de1ae9a4de3aa8b3ae2ffbb533b880ac92c
Diego Gutman <diegogutman@MacBook-Pro-de-Diego.local>
2016-10-08 12:02:48 -0300

    se corrigio reportes

--- resources/views/reports/reports.blade.php
+++ resources/views/reports/reports.blade.php
@@ -11,31 +11,32 @@
                     {{ Form::submit('Buscar') }}
                     {{ Form::close() }}
                 </p>
-
-                <table class="table table-striped table-bordered">
-                    <tr>
-                        <th>Tipo</th>
-                        <th>Motivo</th>
-                        <th>Recorrido</th>
-                        <th>Monto</th>
-                        <th>Responsable</th>
-                        <th>Fecha</th>
-                        <th>Ver</th>
-                    </tr>
-                    @foreach($reports as $report)
-                        <tr>
-                            <td>{{ config('cashier.types')[$report->type] }}</td>
-                            <td>{{ $report->motive }}</td>
-                            <td>{{ $report->recorrido }}</td>
-                            <td>{{ $report->amount }}</td>
-                            <td>{{ $report->responsable }}</td>
-                            <td>{{ \Carbon\Carbon::parse($report->created_at)->format('d/m/y - H:i:s') }}</td>
-                            <td>
-                                <a href="{{route('report.view', $report)}}">Ver</a>
-                            </td>
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered">
+                        <tr class="text-danger">
+                            <th>Tipo</th>
+                            <th>Motivo</th>
+                            <th>Recorrido</th>
+                            <th>Monto</th>
+                            <th>Responsable</th>
+                            <th>Fecha</th>
+                            <th>Acciones</th>
                         </tr>
-                    @endforeach
-                </table>
+                        @foreach($reports as $report)
+                            <tr>
+                                <td>{{ config('cashier.types')[$report->type] }}</td>
+                                <td>{{ $report->motive }}</td>
+                                <td>{{ $report->recorrido }}</td>
+                                <td>{{ number_format($report->amount,2,',','.') }}</td>
+                                <td>{{ $report->responsable }}</td>
+                                <td>{{ \Carbon\Carbon::parse($report->created_at)->format('d/m/y - H:i:s') }}</td>
+                                <td>
+                                    <a href="{{route('report.view', $report)}}">Ver</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </table>
+                </div>
                 {{ $reports->appends(request()->only(['type','created_at']))->render() }}
             </div>
         </div>
--- resources/views/reports/view.blade.php
+++ resources/views/reports/view.blade.php
@@ -4,41 +4,51 @@
     <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
-                <dl class="dl-horizontal">
-                    <dt>Fecha</dt>
-                    <dd>{{ $report->created_at }}</dd>
-                    <br>
+                <div class="panel panel-default">
+                    <div class="panel-heading">Detalle del reporte</div>
+                    <div class="panel-body">
+                        <dl class="dl-horizontal">
+                            <dt class="text-danger">Fecha</dt>
+                            <dd>{{ $report->created_at }}</dd>
+                            <br>
 
-                    <dt>Motivo</dt>
-                    <dd>{{ $report->motive }}</dd>
-                    <br>
+                            <dt class="text-danger">Motivo</dt>
+                            <dd>{{ $report->motive }}</dd>
+                            <br>
 
-                    <dt>Recorrido</dt>
-                    <dd>{{ $report->recorrido }}</dd>
-                    <br>
+                            <dt class="text-danger">Recorrido</dt>
+                            <dd>{{ $report->recorrido }}</dd>
+                            <br>
 
-                    <dt>Billetes</dt>
-                    <dd>
-                        <table class="table">
-                            <tr>
-                                <th>Valor</th>
-                                <th>Cantidad</th>
-                            </tr>
-                        @foreach($detail as $unit => $quantity)
-                            @if($quantity > 0)
-                                <tr>
-                                    <td>{{ $monedas[$unit] }}</td>
-                                    <td>{{ $quantity }}</td>
-                                </tr>
-                            @endif
-                        @endforeach
-                        </table>
-                    </dd>
-                    <br>
-
-                    <dt>Monto</dt>
-                    <dd><strong>{{ $report->amount }}</strong></dd>
-                </dl>
+                            <dt class="text-danger">Detalle</dt>
+                            <dd>
+                                <table class="table table-bordered table-striped">
+                                    <tr class="text-primary">
+                                        <th>Valor</th>
+                                        <th>Cantidad</th>
+                                    </tr>
+                                    @foreach($detail as $unit => $quantity)
+                                        @if($quantity > 0)
+                                            <tr>
+                                                <td><strong>{{ $monedas[$unit] }}</strong></td>
+                                                <td>{{ $quantity }}</td>
+                                            </tr>
+                                        @endif
+                                    @endforeach
+                                    <tr class="text-primary">
+                                        <th>Total</th>
+                                        <th>
+                                            <strong>${{ number_format($report->amount,2,',','.') }}</strong>
+                                        </th>
+                                    </tr>
+                                </table>
+                            </dd>
+                        </dl>
+                    </div>
+                    <div class="panel-footer">
+                        <a href="{{ url()->previous() }}" class="btn btn-default">Volver a la lista</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
