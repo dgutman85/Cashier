e8e568d60cf168311de67aa48dcdcb54940f28cb
Diego Gutman <diegogutman@MacBook-Pro-de-Diego.local>
2016-10-08 11:17:55 -0300

    se agrego fecha a la caja

--- resources/views/boxes/show.blade.php
+++ resources/views/boxes/show.blade.php
@@ -5,7 +5,10 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">{{ $box->name }}</div>
+                    <div class="panel-heading">
+                        {{ $box->name }}
+                        <span class="pull-right">Fecha: {{ \Carbon\Carbon::now()->format('d/m/y') }}</span>
+                    </div>
                     <div id="content"></div>
                 </div>
             </div>
